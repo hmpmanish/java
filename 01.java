741f16323d0a9e2d4a33a92ac15368eefe1e0f82
Manish Pandey <156882775+hmpmanish@users.noreply.github.com>
2026-07-17 09:57:29 +0530

    Add files via upload

---
+++ 01.java
@@ -0,0 +1,21 @@
+class Student 
+{public static void main (String[]arg)
+ {
+     System.out.println("---BIODATA---");
+    System.out.println("NAME\t\t:MANISH PANDEY");
+    System.out.println("AGE\t\t:21");
+    System.out.println("GENDER\t\t:M");
+    System.out.println("CITY\t\t:GONDA");
+
+}
+System.out.println("---formatted output---");
+String name ="manish pandey"
+int age =30;
+String GENDER="male";
+
+System.out,printf("name: %s%n",name);
+System.out.printf("age: %d%n",age);
+
+
+
+}
