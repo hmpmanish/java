35a03ba14b131d2e690484377dcffc5d699dd66b
Manish Pandey <156882775+hmpmanish@users.noreply.github.com>
2026-07-17 10:26:41 +0530

    Add Student class with input for two numbers

---
+++ 03.java
@@ -0,0 +1,16 @@
+
+import java.util.Scanner;
+class Student 
+{
+public static void main(String[] args)
+{ int a,b;
+        Scanner sc = new Scanner(System.in);
+
+System.out.println("ENTER THE ENTER THE FIRST NNO");
+ a=sc.nextInt();
+ System.out.println("ENTER THE 2nd NO");
+ b=sc.nextInt();
+
+
+}
+}
