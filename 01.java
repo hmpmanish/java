b9c934615c5156066ceb71e1e050a186d6d5d28e
Manish Pandey <156882775+hmpmanish@users.noreply.github.com>
2026-07-23 15:31:49 +0530

    Refactor Student class for improved output formatting

--- 01.java
+++ 01.java
@@ -1,21 +1,20 @@
-class Student 
-{public static void main (String[]arg)
- {
-     System.out.println("---BIODATA---");
-    System.out.println("NAME\t\t:MANISH PANDEY");
-    System.out.println("AGE\t\t:21");
-    System.out.println("GENDER\t\t:M");
-    System.out.println("CITY\t\t:GONDA");
+class Student {
+    public static void main(String[] args) {
 
-}
-System.out.println("---formatted output---");
-String name ="manish pandey"
-int age =30;
-String GENDER="male";
+        System.out.println("---BIODATA---");
+        System.out.println("NAME\t\t: MANISH PANDEY");
+        System.out.println("AGE\t\t: 21");
+        System.out.println("GENDER\t\t: M");
+        System.out.println("CITY\t\t: GONDA");
 
-System.out,printf("name: %s%n",name);
-System.out.printf("age: %d%n",age);
+        System.out.println("\n---Formatted Output---");
 
+        String name = "Manish Pandey";
+        int age = 30;
+        String gender = "Male";
 
-
+        System.out.printf("Name: %s%n", name);
+        System.out.printf("Age: %d%n", age);
+        System.out.printf("Gender: %s%n", gender);
+    }
 }
