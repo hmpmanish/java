4a6128fe5bcc4e68b641aa7266fa785ec5fc0ed5
Manish Pandey <156882775+hmpmanish@users.noreply.github.com>
2026-07-24 09:48:23 +0530

    Add basic calculator functionality in 07.java
    Implemented basic calculator functionality with addition, subtraction, multiplication, and division options. Added user input for selecting operations and entering numbers.

---
+++ calculator functionality in 07.java
@@ -0,0 +1,61 @@
+import java.util.Scanner;
+class Calcuator{
+    public static void main(String[]arg){
+        Scanner sc = new Scanner(System.in);
+        int n1,n2,choice;
+        System.out.println("1.Addition");
+        System.out.println("2.Subtracton");
+        System.out.println("3.Multiplication");
+        System.out.println("4.Division");
+        System.out.println("Enter you choice");
+        choice=sc.nextInt();
+        switch(choice)
+        {
+            case 1:
+            int result1 =n1+n2;
+                    System.out.print("result1");
+                    break;
+                    
+                    
+                    
+                    
+                    
+                      case 2:
+            int result2 =n1-n2;
+                    System.out.print("result2");
+                    break;
+                    
+                    
+                    
+                    
+                      case 3 :
+            int result3 =n1*n2;
+                    System.out.print("result3");
+                    break;
+                    
+                    
+                    
+                    
+                      case 4:
+            int result4 =n1/n2;
+                    System.out.print("result4");
+                    break;
+                    
+                    default:
+                            System.out.print("ls enter valid choice");
+                            sc.close();
+                    
+
+            
+        }
+        
+        
+        
+        System.out.print("Enter the first no:");
+        n1=sc.nextInt();
+        System.out.print("Enter the second no:");
+        n2=sc.nextInt();
+        
+        
+    }
+}
