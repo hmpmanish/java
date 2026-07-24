d5e4410c34a0231393b1670059a29f437c6fe060
Manish Pandey <156882775+hmpmanish@users.noreply.github.com>
2026-07-24 10:07:31 +0530

    Create Student class with array input functionality
    Implemented a Student class that takes an array input from the user.

---
+++ 08.Create Student class with array input functionality.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+class Student{
+    public static void main(String[]arg){
+        Scanner sc = new Scanner(System.in);
+        int n;
+                System.out.println("Enter the size of array");
+                n=sc.nextInt();
+                int[]arr=new int[n];
+                        System.out.println("enter the elements");
+
