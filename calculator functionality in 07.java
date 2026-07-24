fea10285c630d50c5c37e5f19bd3b69e5670b41b
Manish Pandey <156882775+hmpmanish@users.noreply.github.com>
2026-07-24 09:57:42 +0530

    Improve calculator input handling and error messages
    Refactor calculator functionality to improve user prompts and handle division by zero correctly.

--- calculator functionality in 07.java
+++ calculator functionality in 07.java
@@ -7,64 +7,41 @@ class Calcuator{
         System.out.println("2.Subtracton");
         System.out.println("3.Multiplication");
         System.out.println("4.Division");
-        System.out.println("Enter you choice");
+        System.out.print("Enter you choice:");
         choice=sc.nextInt();
-        
-        
         System.out.print("Enter the first no:");
         n1=sc.nextInt();
         System.out.print("Enter the second no:");
         n2=sc.nextInt();
         
-        
-        switch(choice)
-        {
+        switch(choice) {
             case 1:
-            int result1 =n1+n2;
-                    System.out.print(result1);
-                    break;
-                    
-                    
-                    
-                    
-                    
-                      case 2:
-            int result2 =n1-n2;
-                    System.out.print(result2);
-                    break;
-                    
-                    
-                    
-                    
-                      case 3 :
-            int result3 =n1*n2;
-                    System.out.print(result3);
-                    break;
-                    
-                    
-                    
-                    
-                      case 4:
-                          if(n2==0){
-                                                     System.out.print("div is not possible due to vlue of n2");
-    
-                              
-                              
-                          }
-            int result4 =n1/n2;
-                    System.out.print(result4);
-                    break;
-                    
-                    default:
-                            System.out.print("ls enter valid choice");
-                            sc.close();
-                    
-
+                int result1 =n1+n2;
+                System.out.print(result1);
+                break;   
+            case 2:
+                int result2 =n1-n2;
+                System.out.print(result2);
+                break;  
+            case 3:
+                int result3 =n1*n2;
+                System.out.print(result3);
+                break;  
+            case 4:
+                if(n2==0){
+                    System.out.print("Division is not possible due to value of n2.");
+                }
+                else{
+                int result4 =n1/n2;
+                System.out.println(result4);
+                }
+                break;
+                
             
+            default :
+                System.out.println("Please enter the valid choice");
+            sc.close();    
         }
-        
-        
-        
         
         
         
